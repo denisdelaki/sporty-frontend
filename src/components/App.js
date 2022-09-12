0f8d12e6f44fa9169ba6fdadbb91ce121079a555
denisdelaki <denisdelaki@gmail.com>
2022-09-12 12:29:11 +0300

    linked the components together and to the parent component App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../App.css';
+import Header from './Header';
 import Home from './Home';
+import Login from './Login';
 function App() {
 
     return (
         <>
-        <Home />
+            <Login />
+            <Header />
+            <Home />
         </>
     )
 
