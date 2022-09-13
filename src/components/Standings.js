f2585f122dfdba4d415876b941e75ee0a9d7182d
denisdelaki <denisdelaki@gmail.com>
2022-09-13 21:21:15 +0300

    created a card to accomodate the standings

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -8,7 +8,59 @@ function Standings() {
             .then(standing => {
             setStandings(standing)
         })
-    })
+    }, [])
+    const gameStanding = Object.keys(standings).map((standing) => (
+      <div>
+        <div>
+          <table>
+            <tr>
+              <th>Team ID</th>
+              <th>Team Name</th>
+              <th>League Name</th>
+            </tr>
+            <tr>
+              <td>{standings[standing].team_id}</td>
+              <td>{standings[standing].team_name}</td>
+              <td>{standings[standing].league_name}</td>
+            </tr>
+          </table>
+          <img src={standings[standing].team_badge} alt="Badge" />
+          <table>
+            <tr>
+              <th>Home</th>
+              <th>Away</th>
+              <th>Overall</th>
+            </tr>
+            <tr>
+              <p>
+                League Losses:
+                <td>{standings[standing].home_league_L}</td>
+              </p>
+              <p>
+                League Wins:
+                <td>{standings[standing].home_league_W}</td>
+              </p>
+              <p>
+                League Points:
+                <td>{standings[standing].home_league_PTS}</td>
+              </p>
+              <p>
+                League GA:
+                <td>{standings[standing].home_league_GA}</td>
+              </p>
+              <p>
+                League GF:
+                <td>{standings[standing].home_league_GF}</td>
+              </p>
+              <p>
+                League Position:
+                <td>{standings[standing].home_league_position}</td>
+              </p>
+            </tr>
+          </table>
+        </div>
+      </div>
+    ));
     return (
         <div>
             standing
