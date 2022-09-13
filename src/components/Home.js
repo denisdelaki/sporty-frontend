9cf5716cbaacf7edd727e543db524517a7343ac4
denisdelaki <denisdelaki@gmail.com>
2022-09-13 19:16:49 +0300

    added the countries component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
+// import { NavLink } from "react-router-dom";
+import "../App.css";
+import Countries from "./Countries";
 function Home() {
    
+
+
+
     return (
-        <>
-        <p>Home</p>
-        </>
-    )
+      <div className="home">
+        <div className="card">
+          <p className="hide"> Hide </p>
+                <nav>
+                {/* <NavLink to="/">Countries</NavLink> */}
+                </nav> 
+         
+        </div>
+        <div className="view">
+                <div>
+                  <Countries />
+                </div> 
+        </div>
+        <div className="slip">
+          <h2 className="sliphead">Bet Slip</h2>
+        </div>
+      </div>
+    );
 }
 export default Home
