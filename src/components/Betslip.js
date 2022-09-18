43ed8cb40dbd9ee12ce357de84c5d0a00b53f3f7
denisdelaki <denisdelaki@gmail.com>
2022-09-18 22:48:05 +0300

    created the betslip component and fetched the data from the public Api

---
+++ src/components/Betslip.js
@@ -0,0 +1,8 @@
+import React from "react"; 
+function Betslip() {
+     
+     return (<div>
+        
+    </div>) 
+ }
+ export default Betslip
--- src/components/Prediction.js
+++ src/components/Prediction.js
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from "react";
+import Betslip from "./Betslip";
 
 function Prediction() {
-    const [predictions, setPrediction] = useState([])
-    useEffect(() => {
-        let today = new Date
-        let currentDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-        let tommorrow =
-          today.getFullYear() +
-          "-" +
-          (today.getMonth() + 1) +
-          "-" +
-          (today.getDate()+1);
-       
-        fetch(`https://apiv3.apifootball.com/?action=get_predictions&from=${currentDate}&to=${tommorrow}&APIkey=e3d29ea6462a794ef963d228f1737e2ac2c9a5f86c085b948f1127fd8e75a17c`)
-            .then(res => res.json())
-            .then((predictions) => {
-            setPrediction(predictions)
-        })
+  const [predictions, setPrediction] = useState([])
+  const [betslip, setBetslip]=useState({})
+  
+  let today = new Date();
+  let currentDate =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let tommorrow =
+    today.getFullYear() +
+    "-" +
+    (today.getMonth() + 1) +
+    "-" +
+    (today.getDate() + 1);
+  function handleBet() {
+    //   console.log(bet)
         
-    }, [])
+  }
+  useEffect(() => {
+       
+    fetch(`https://apiv3.apifootball.com/?action=get_predictions&from=${currentDate}&to=${tommorrow}&APIkey=e3d29ea6462a794ef963d228f1737e2ac2c9a5f86c085b948f1127fd8e75a17c`)
+      .then(res => res.json())
+      .then((predictions) => {
+        setPrediction(predictions)
+      })
+        
+  }, [])
+  useEffect(() => {
+    fetch("http://localhost:3000/betslip", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(predictions),
+    })
+      .then((res) => res.json())
+      .then(betslip => {
+        setBetslip(...betslip)
+      })
+  }, [])
     const bet = Object.keys(predictions).map((Prediction) => (
       <div key={predictions[Prediction].match_id} className="bettable">
         <table>
@@ -28,7 +49,7 @@ function Prediction() {
             <td>
               Status:{" "}
               {
-                (predictions[Prediction].match_status = ""
+                (predictions[Prediction].match_status = " "
                   ? "To Start"
                   : predictions[Prediction].match_status)
               }
@@ -69,7 +90,16 @@ function Prediction() {
           </tr>
           <tr>
             <td>
-              <button>{predictions[Prediction].prob_HW / 100}</button>
+              <button
+                onClick={() =>
+                  handleBet(
+                    predictions[Prediction],
+                    predictions[Prediction].prob_HW / 100
+                  )
+                }
+              >
+                {predictions[Prediction].prob_HW / 100}
+              </button>
             </td>
             <td>
               <button>{predictions[Prediction].prob_D / 100}</button>
@@ -96,8 +126,11 @@ function Prediction() {
         </table>
       </div>
     ));
-    return (<div className="bet">
-       {bet}
-    </div>)
+  return (
+    <div>
+      <div className="bet">{bet}</div>
+      <Betslip  />
+    </div>
+  );
 }
 export default Prediction 
